e7d44552b91b0a5442a803893e40802c3df0ce73
Roshan George <roshan@arjie.com>
2014-05-03 21:20:00 -0400

    Explain calling convention

--- src/nothing.s
+++ src/nothing.s
@@ -1,8 +1,11 @@
 .text
 .globl _start
 _start:
-  subq $8, %rsp
+  subq $8, %rsp # Re-align stack pointer to a multiple of 16 bytes.
 
-  movq $60, %rax
-  movq $1, %rdi
+  movq $60, %rax # Choose sys_exit syscall
+  movq $1, %rdi # Set error code to 1
+  # integer arguments are passed in the following sequence of registers
+  # %rdi, %rsi, %rdx, %rcx, %r8, %r9, then the rest are pushed on the stack
+  # in reverse order
   syscall
