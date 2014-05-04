89f8711e7ec379856761e7d0ff283ca147a42e1d
Roshan George <roshan@arjie.com>
2014-05-03 21:32:16 -0400

    String substitution with printf

--- src/hello.s
+++ src/hello.s
@@ -1,14 +1,15 @@
 .data
 _hello:
-  .asciz "Hello, world\n" # .asciz - string with trailing zero byte
+  .asciz "Hello, world: %d\n" # .asciz - string with trailing zero byte
 
 .text
 .globl _start
 _start:
   subq $8, %rsp
 
-  movb $0, %al
-  leaq _hello(%rip), %rdi
+  movb $0, %al # specify the number of vector arguments to printf
+  leaq _hello(%rip), %rdi # load the string
+  movq $24, %rsi # load the number 24 as the second param
   call printf
 
   movq $0, %rdi
